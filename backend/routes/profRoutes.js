49c3c580626fbf35d3d26137fee23782f8226657
Erick Lopes <erickarte@hotmail.com>
2025-04-24 20:04:27 -0300

    restaurando

--- backend/routes/profRoutes.js
+++ backend/routes/profRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createProf, getProfs, login, updateProf, deleteProf, getProfById, getProfsById } = require('../controllers/profController');
+const { createProf, getProfs, login, updateProf, deleteProf, getProfById, getProfById } = require('../controllers/profController');
 
 // Rotas de usuários
 router.post('/', createProf);
 router.get('/', getProfs);
-router.get('/:id', getProfsById);
+router.get('/:id', getProfById);
 router.post('/login', login);
 router.put('/:id', updateProf); // Rota para atualizar usuário
 router.delete('/:id', deleteProf); // Rota para deletar usuário
